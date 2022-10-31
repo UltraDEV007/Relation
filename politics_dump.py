9285d33f796bc9a084c4d194ce7ccc1739b5f6e8
UltraDEV007 <LeTuan2024@outlook.com>
2022-10-31 20:54:15 +0800

    fix: create new connection everytime when the dump is running

--- politics_dump.py
+++ politics_dump.py
@@ -4,17 +4,16 @@ import os
 import csv
 from tools.uploadGCS import upload_blob
 
-db = os.environ['DATABASE']
-db_user = os.environ['DATABASE_USER']
-db_pw = os.environ['DATABASE_PASSWORD']
-db_host = os.environ['DATABASE_HOST']
-db_port = os.environ['DATABASE_PORT']
-
-connection = psycopg2.connect(database=db, user=db_user,password=db_pw, host=db_host, port=db_port)
-cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
-
 def dump_politics(election_id):
-    print(election_id)
+    db = os.environ['DATABASE']
+    db_user = os.environ['DATABASE_USER']
+    db_pw = os.environ['DATABASE_PASSWORD']
+    db_host = os.environ['DATABASE_HOST']
+    db_port = os.environ['DATABASE_PORT']
+
+    connection = psycopg2.connect(database=db, user=db_user,password=db_pw, host=db_host, port=db_port)
+    cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
     dump_query = """SELECT "desc", "content", "Person"."name", "Election"."name", "ElectionArea"."name", "Organization"."name" FROM "Politic", "PersonElection", "Person", "Election", "ElectionArea", "Organization" WHERE "Politic".person = "PersonElection".id AND "PersonElection".election = "Election"."id" AND "PersonElection"."person_id" = "Person"."id" AND "ElectionArea"."id" = "PersonElection"."electoral_district" AND "Organization"."id" = "PersonElection"."party" AND "Politic"."status" = 'verified' AND "Election".id = """ + str(election_id)
     cursor.execute(dump_query)
     all_politics = cursor.fetchall()
@@ -27,6 +26,8 @@ def dump_politics(election_id):
         writer.writerow(['政見', '政見補充', '候選人', '選舉', '選區', '政黨'])
         writer.writerows(all_politics)
         upload_blob(destination_file, 2022)
+
+    connection.close()
 
 if __name__=="__main__":
     dump_politics(82)
