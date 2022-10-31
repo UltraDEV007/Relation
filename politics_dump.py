ca8dc717a8095fe7906e2cf6f3ab7214cbc83b49
UltraDEV007 <LeTuan2024@outlook.com>
2022-10-31 20:23:11 +0800

    fix: correct the destination_file

--- politics_dump.py
+++ politics_dump.py
@@ -18,12 +18,12 @@ def dump_politics(election_id):
     dump_query = """SELECT "desc", "content", "Person"."name", "Election"."name", "ElectionArea"."name", "Organization"."name" FROM "Politic", "PersonElection", "Person", "Election", "ElectionArea", "Organization" WHERE "Politic".person = "PersonElection".id AND "PersonElection".election = "Election"."id" AND "PersonElection"."person_id" = "Person"."id" AND "ElectionArea"."id" = "PersonElection"."electoral_district" AND "Organization"."id" = "PersonElection"."party" AND "Politic"."status" = 'verified' AND "Election".id = """ + str(election_id)
     cursor.execute(dump_query)
     all_politics = cursor.fetchall()
+    destination_file = 'politics/' + str(election_id) + ".csv"
 
-    with open('output.csv', 'w', newline='') as csvfile:
+    with open(destination_file, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(['政見', '政見補充', '候選人', '選舉', '選區', '政黨'])
         writer.writerows(all_politics)
-        destination_file = 'politics/' + str(election_id) + ".csv"
         upload_blob(destination_file, 2022)
 
 if __name__=="__main__":
