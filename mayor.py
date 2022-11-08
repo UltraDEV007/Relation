829edfb1ef864ac347a0296deca3d27be763a9c6
UltraDEV007 <LeTuan2024@outlook.com>
2022-11-08 12:24:39 +0800

    refactor: update for  new mock data and rm chayi city  in mayor

--- mayor.py
+++ mayor.py
@@ -11,20 +11,15 @@ with open('mapping/mapping_county_town_vill.json') as f:
     mapping_county_town_vill = json.loads(f.read())
 with open('mapping/mayor_candidate_2022.json') as f:
     candidate_info = json.loads(f.read())
-VOTES = 'prof3'
-ELEGIBLE_VOTERS = 'prof7'
 ENV_FOLDER = os.environ['ENV_FOLDER']
+
 
 def parse_cec_mayor(data):
     organized_data = {}
     for district in data:
         deptCode = district['deptCode'] if district['deptCode'] else '000'
         region_code = f"{district['prvCode']}_{district['cityCode']}_{deptCode}"
-        region = organized_data.setdefault(region_code, {'detailed': {
-            VOTES: district[VOTES],
-            ELEGIBLE_VOTERS: district[ELEGIBLE_VOTERS],
-            'profRate': district['profRate'],
-        }})
+        region = organized_data.setdefault(region_code, {'profRate': district['profRate']})
         for c in district['candTksInfo']:
             candNo = region.setdefault(c['candNo'], c)
             candNo = c
@@ -101,13 +96,16 @@ def gen_vote(polling_data='', candidate_info=candidate_info, year=datetime.now()
                 'candVictor': False
             }
             if polling_data:
-                candTks['tks'] = polling_data[region_code][int(
-                    candNo)]['tks'] if polling_data[region_code][int(candNo)]['tks'] else 0
-                candTks['tksRate'] = polling_data[region_code][int(
-                    candNo)]['tksRate'] if polling_data[region_code][int(candNo)]['tksRate'] else 0
-                candTks['candVictor'] = True if polling_data[region_code][int(
-                    candNo)]['candVictor'] == '*' or polling_data[region_code][int(
-                    candNo)]['candVictor'] == True else False
+                try:
+                    candTks['tks'] = polling_data[region_code][int(
+                        candNo)]['tks'] if polling_data[region_code][int(candNo)]['tks'] else 0
+                    candTks['tksRate'] = polling_data[region_code][int(
+                        candNo)]['tksRate'] if polling_data[region_code][int(candNo)]['tksRate'] else 0
+                    candTks['candVictor'] = True if polling_data[region_code][int(
+                        candNo)]['candVictor'] == '*' or polling_data[region_code][int(
+                        candNo)]['candVictor'] == True else False
+                except KeyError:
+                    pass
             candidates.append(candTks)
         try:
             districtName = mapping_county_town[region_code]
@@ -143,10 +141,13 @@ def map_candidate(region_candidates, polling_data, region_code):
             "candVictor": " "
         }
         if polling_data:
-            can_polling_data = polling_data[region_code][int(candNo)]
-            candTks['tks'] = can_polling_data['tks'] if can_polling_data['tks'] else 0
-            candTks['tksRate'] = can_polling_data['tksRate'] if can_polling_data['tksRate'] else 0
-            candTks['candVictor'] = can_polling_data['candVictor'] if can_polling_data['candVictor']else ' '
+            try:
+                can_polling_data = polling_data[region_code][int(candNo)]
+                candTks['tks'] = can_polling_data['tks'] if can_polling_data['tks'] else 0
+                candTks['tksRate'] = can_polling_data['tksRate'] if can_polling_data['tksRate'] else 0
+                candTks['candVictor'] = can_polling_data['candVictor'] if can_polling_data['candVictor']else ' '
+            except KeyError:
+                pass
         candidates.append(candTks)
 
     return candidates
@@ -154,8 +155,6 @@ def map_candidate(region_candidates, polling_data, region_code):
 
 def gen_map(scope, polling_data,  scope_code='', sub_region=''):
     result = []
-    country_votes = 0
-    country_eligible_voters = 0
     for region_code in sub_region.keys():
         if scope == 'country':
             vill_Code = '000'
@@ -166,7 +165,7 @@ def gen_map(scope, polling_data,  scope_code='', sub_region=''):
             range = f'{mapping_county_town[scope_code]} {mapping_county_town[region_code]}'
         else:
             vill_Code = region_code
-            region_code = scope_code +'_' + region_code  # vill code '09_007_010_010'
+            region_code = scope_code + '_' + region_code  # vill code '09_007_010_010'
             range = sub_region[vill_Code].replace("_", " ")
 
         region_code_split = region_code.split('_')
@@ -175,14 +174,10 @@ def gen_map(scope, polling_data,  scope_code='', sub_region=''):
 
         candidates = map_candidate(candidate_info[f"{county_code}_000"], polling_data, region_code)
         if polling_data:
-            detailed_polling_data = polling_data[region_code]['detailed']
-            profRate = detailed_polling_data['profRate'] if detailed_polling_data['profRate'] else 0
-            if scope == 'country': 
-                country_votes += detailed_polling_data[VOTES]
-                country_eligible_voters += detailed_polling_data[ELEGIBLE_VOTERS]
+            profRate = polling_data[region_code]['profRate'] if polling_data[region_code]['profRate'] else 0
         else:
             profRate = 0
-        
+
         result.append({
             "range": range,
             "county": county_code.replace('_', ''),
@@ -195,13 +190,6 @@ def gen_map(scope, polling_data,  scope_code='', sub_region=''):
     data = {"updatedAt": now,
             "districts": result}
     if scope == 'country':
-        data['summary'] = {
-            "range": "全國",
-            "county": None,
-            "town": None,
-            "vill": None,
-            "profRate": round(country_votes / country_eligible_voters * 100, 2) if polling_data else 0
-        }
         destination_file = f'{ENV_FOLDER}/{year}/mayor/map/{scope}.json'
     elif scope == 'county':
         destination_file = f'{ENV_FOLDER}/{year}/mayor/map/{scope}/{scope_code[:-3].replace("_", "")}.json'
@@ -210,7 +198,6 @@ def gen_map(scope, polling_data,  scope_code='', sub_region=''):
 
     save_file(destination_file, dict(sorted(data.items(), reverse=True)), year)
     return
-    
 
 
 def gen_mayor(data = ''):
@@ -219,20 +206,23 @@ def gen_mayor(data = ''):
     gen_vote(data)
     gen_map('country', data, '00_000_000', candidate_info)
     for county_code, towns in mapping_county_town_vill.items():
+        if county_code == '10_020':#2022嘉義市長選舉延後
+            continue
         county_code = county_code + '_000'
         print(mapping_county_town[county_code])
         gen_map('county', data, county_code, towns)
         if os.environ['isSTARTED'] != 'true':
             for town_code, vills in towns.items():
-                    town_code = county_code[:-3] + town_code
-                    print(mapping_county_town[town_code])
-                    gen_map('town', polling_data='', scope_code = town_code, sub_region=vills )
+                town_code = county_code[:-3] + town_code
+                # print(mapping_county_town[town_code])
+                gen_map('town', polling_data='',
+                        scope_code = town_code, sub_region=vills)
     return
 
 
 if __name__ == '__main__':
     if os.environ['isSTARTED'] == 'true':
-        jsonfile = request_cec('running.json')
+        # jsonfile = request_cec('running.json')
         with open('running.json') as f:
             jsonfile = json.loads(f.read())
         if jsonfile:
