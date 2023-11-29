e08bb6f6069dfd814a65452f6cd2f9606eaa1f77
UltraDEV007 <LeTuan2024@outlook.com>
2023-11-29 17:53:43 +0800

    test: fetch cec data 2024, disable SSL verification

--- tools/cec_data.py
+++ tools/cec_data.py
@@ -11,7 +11,8 @@ def check_cec_2024():
     filename = "running.json"
     url = f"{CECURL_2024}{filename}"
     
-    r = requests.get(url=url, auth=(os.environ['USERNAME_2024'], os.environ['PASSWD_2024']))
+    ### We disable SSL certificate authentication here...but it might need...
+    r = requests.get(url=url, auth=(os.environ['USERNAME_2024'], os.environ['PASSWD_2024']), verify=False)
     try:
         r.raise_for_status()
     except requests.exceptions.HTTPError:
