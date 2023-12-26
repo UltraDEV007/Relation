37cbfb5afb6554b3caf6f9c7cb9d5cf03f89e9ea
UltraDEV007 <LeTuan2024@outlook.com>
2023-12-26 13:08:56 +0800

    feat: add the updateAt for the 2024 election homepage data

--- data_export.py
+++ data_export.py
@@ -24,10 +24,10 @@ def president2024_realtime():
     readr_data = {}
 #    if switch_view == 'T' or get_cec_data == 'T':
     cec_json= requests.get('https://whoareyou-gcs.readr.tw/elections-dev/2024/president/map/country/country.json')
-    readr_data["updateAt"] = cec_json["updateAt"]
-    voting_data["updateAt"] = cec_json["updateAt"]
     if cec_json.status_code == 200:
         cec_data = json.loads(cec_json.text)
+        readr_data["updateAt"] = cec_json["updateAt"]
+        voting_data["updateAt"] = cec_json["updateAt"]
         # upload for pure cec data
         readr_data["title"] = "2024 總統大選即時開票"
         readr_data["result"] = presindent2024_cec( cec_data["summary"], 2 )
