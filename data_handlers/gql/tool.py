7741ed6e56894e9c92f1694030d3685acbb73cf3
UltraDEV007 <LeTuan2024@outlook.com>
2024-01-17 16:20:38 +0800

    feat: update cms president

--- data_handlers/gql/tool.py
+++ data_handlers/gql/tool.py
@@ -16,5 +16,5 @@ def gql_update(gql_endpoint, gql_string, gql_variable):
     gql_transport = AIOHTTPTransport(url=gql_endpoint)
     gql_client = Client(transport=gql_transport,
                         fetch_schema_from_transport=True)
-    json_data = gql_client.execute_async(gql(gql_string), variable_values=gql_variable)
+    json_data = gql_client.execute(gql(gql_string), variable_values=gql_variable)
     return json_data
